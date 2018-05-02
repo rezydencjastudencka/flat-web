c09637138eed62074140aec9dda9ae7dd4420e90
Lukasz Gondela <ishfid4@gmail.com>
2018-05-02 21:04:02 +0200

    Add handling login errors and success
    Add handling login errors, routing to another page when succesful
    and snackbar when login failed

--- src/services/FlatApi.js
+++ src/services/FlatApi.js
@@ -1,13 +1,21 @@
+import Router from '../router';
+
 const flatApiUrl = 'http://dev.api.flat.kubernetes.tobiasz.memleak.pl/';
 
 export default {
-  logIn(credentials) {
+  logIn(credentials, showErrorMessage) {
     fetch(`${flatApiUrl}/session/create`, {
       method: 'POST',
       body: JSON.stringify(credentials),
       headers: new Headers({
         'Content-Type': 'application/json',
       }),
-    }).then(res => res.json());
+    }).then((response) => {
+      if (response.status === 200) {
+        Router.push('/');
+      } else {
+        showErrorMessage();
+      }
+    });
   },
 };
