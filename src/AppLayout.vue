8d70754dec963e515c3aa2a3e04ae90692994eb2
Lukasz Gondela <ishfid4@gmail.com>
2018-05-03 12:26:12 +0200

    Add toolbar and nav drawer to layout template
    Signed-off-by: Lukasz Gondela <ishfid4@gmail.com>

--- src/AppLayout.vue
+++ src/AppLayout.vue
@@ -1,12 +1,57 @@
 <template>
   <div>
-    <router-view/>
+    <v-navigation-drawer
+      fixed
+      v-model="drawer"
+      app
+    >
+      <v-list class="pa-1">
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img src="https://ssl.gstatic.com/images/branding/product/1x/avatar_square_blue_512dp.png" >
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ userName }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+        <v-list-tile v-for="item in items" :key="item.title" @click="item.action">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar>
+      <v-toolbar-side-icon
+        @click.stop="drawer = !drawer"
+      ></v-toolbar-side-icon>
+      <v-toolbar-title>Flat</v-toolbar-title>
+    </v-toolbar>
+    <v-content><router-view/></v-content>
   </div>
 </template>
 
 <script>
 export default {
   name: 'AppLayout',
+  data() {
+    return {
+      drawer: null,
+      userName: 'RandomUser',
+      items: [
+        { title: 'Expenses', icon: 'trending_down', action: '/expenses' },
+        { title: 'Incomes', icon: 'trending_up', action: '/incomes' },
+        { title: 'Transfers', icon: 'compare_arrows', action: '/transfers' },
+        { title: 'About', icon: 'question_answer', action: '/about' },
+      ],
+    };
+  },
 };
 </script>
 
