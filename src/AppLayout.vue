be4133544df9568cf629f3cc66fb3ac4ddf3ca39
Lukasz Gondela <ishfid4@gmail.com>
2018-05-03 12:26:12 +0200

    Add toolbar and nav drawer to layout template
    Signed-off-by: Lukasz Gondela <ishfid4@gmail.com>

--- src/AppLayout.vue
+++ src/AppLayout.vue
@@ -1,14 +1,90 @@
 <template>
   <div>
-    <router-view/>
+    <v-navigation-drawer
+      app
+      mobileBreakPoint="1"
+      clipped
+      stateless
+      fixed
+      :mini-variant="isNavDrawerClosed"
+      permanent
+    >
+      <v-toolbar flat class="transparent" height="64">
+        <v-list class="pa-0">
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <img :src="userAvatar" >
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ userName }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+        <v-list-tile v-for="item in items" :key="item.title" @click="item.action">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+      <v-list class="pt-0 nav-toggle-button" dense>
+        <v-list-tile @click.native.stop="isNavDrawerClosed = !isNavDrawerClosed">
+          <v-list-tile-action>
+            <v-icon :class="!isNavDrawerClosed?'rotated':''"> forward </v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Toggle drawer</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar clippedLeft app fixed flat height="64">
+      <logo class="logo"/>
+      <v-toolbar-title>Flat</v-toolbar-title>
+    </v-toolbar>
+    <v-content><router-view/></v-content>
   </div>
 </template>
 
 <script>
+import Logo from '@/components/Logo';
+
 export default {
   name: 'AppLayout',
+  components: {
+    logo: Logo,
+  },
+  data() {
+    return {
+      userName: 'Random User',
+      userAvatar: 'https://i.stack.imgur.com/34AD2.jpg',
+      items: [
+        { title: 'Expenses', icon: 'trending_down', action: () => true },
+        { title: 'Incomes', icon: 'trending_up', action: () => true },
+        { title: 'Transfers', icon: 'compare_arrows', action: () => true },
+        { title: 'About', icon: 'question_answer', action: () => true },
+      ],
+      isNavDrawerClosed: true,
+    };
+  },
 };
 </script>
 
 <style>
+.rotated{
+  transform: rotate(180deg);
+}
+.logo{
+  height: 2em;
+  width: 2em;
+}
+.nav-toggle-button{
+  position: absolute;
+  bottom: 0;
+}
 </style>
