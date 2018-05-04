22c9d27cadb502c81701cc3ae95e9b8b65abe88e
Lukasz Gondela <ishfid4@gmail.com>
2018-05-04 21:08:19 +0200

    Add date picker, loading effect in table while fetching data
    Signed-off-by: Lukasz Gondela <ishfid4@gmail.com>

--- src/components/Expenses.vue
+++ src/components/Expenses.vue
@@ -1,14 +1,27 @@
 <template>
   <v-container>
+    <v-dialog v-model="datePicker.isShown" persistent max-width="290">
+      <v-btn color="primary" dark slot="activator">Pick month</v-btn>
+      <v-card>
+        <v-date-picker ref="picker" v-model="datePicker.date" type="month"></v-date-picker>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" flat @click.native="datePicker.isShown = false">Cancel</v-btn>
+          <v-btn color="primary" flat @click.native=setDateToQueryVariables()>Set</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <v-layout align-center justify-center>
       <v-flex xs12 sm10 md8>
       <v-data-table
         :headers="headers"
         :items="expenses"
+        :loading="$apollo.loading"
         disable-initial-sort
         hide-actions
         class="elevation-2"
       >
+        <v-progress-linear slot="progress" color="primary" indeterminate></v-progress-linear>
         <template slot="items" slot-scope="props">
           <td>{{ props.item.name }}</td>
           <td class="text-xs-right">{{ props.item.amount }}</td>
@@ -23,6 +36,7 @@
 
 <script>
 import gql from 'graphql-tag';
+import moment from 'moment';
 
 export default {
   name: 'expenses',
@@ -40,10 +54,13 @@ export default {
         }
       }
     }`,
-      // Static parameters
-      variables: {
-        year: 2018,
-        month: 2,
+      // Reactive parameters
+      variables() {
+        // Use vue reactive properties here
+        return {
+          year: this.selectedDate.year(),
+          month: this.selectedDate.month() + 1,
+        };
       },
     },
   },
@@ -56,7 +73,18 @@ export default {
         { text: 'From', value: 'from' },
       ],
       expenses: [],
+      datePicker: {
+        isShown: false,
+        date: null,
+      },
+      selectedDate: moment(),
     };
+  },
+  methods: {
+    setDateToQueryVariables() {
+      this.selectedDate = moment(this.datePicker.date, 'YYYY-MM');
+      this.datePicker.isShown = false;
+    },
   },
 };
 </script>
