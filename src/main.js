7d154bd13a1d5943f654333832f904527b2488cd
Radoslaw Pieja <pieja.radoslaw@gmail.com>
2018-05-04 20:42:29 +0200

    Add vuex state store, including usage example.
    Add vuex to project,
    Create state store for app,
    Inject store instance to all components,
    Add example state for reference
    
    Signed-off-by: Radoslaw Pieja <pieja.radoslaw@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,16 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'es6-promise/auto';
+
 import Vue from 'vue';
+import Vuex from 'vuex';
 import Vuetify from 'vuetify';
 import App from './App';
 import router from './router';
+import stateStore from './stateStore';
 
 Vue.use(Vuetify);
+Vue.use(Vuex);
 Vue.config.productionTip = false;
+
+// console.log("start create vuex store");
+const store = stateStore.createStore();
+// console.log("dupa");
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
   template: '<App/>',
