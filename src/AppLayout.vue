2d1905f2b9eab0d603f5ced5e561451c4165d034
Mateusz Nowotyński <maxmati4@gmail.com>
2018-08-30 23:49:25 +0200

    Add links to revenues and expenses in menu
    Signed-off-by: Mateusz Nowotyński <maxmati4@gmail.com>

--- src/AppLayout.vue
+++ src/AppLayout.vue
@@ -13,7 +13,7 @@
         <v-list class="pa-0">
           <v-list-tile avatar>
             <v-list-tile-avatar>
-              <img :src="userAvatar" >
+              <img :src="userAvatar">
             </v-list-tile-avatar>
             <v-list-tile-content>
               <v-list-tile-title>{{ userName }}</v-list-tile-title>
@@ -35,7 +35,7 @@
       <v-list class="pt-0 nav-toggle-button" dense>
         <v-list-tile @click.native.stop="isNavDrawerClosed = !isNavDrawerClosed">
           <v-list-tile-action>
-            <v-icon :class="!isNavDrawerClosed?'rotated':''"> forward </v-icon>
+            <v-icon :class="!isNavDrawerClosed?'rotated':''"> forward</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title>Toggle drawer</v-list-tile-title>
@@ -44,17 +44,19 @@
       </v-list>
     </v-navigation-drawer>
     <v-toolbar clippedLeft app fixed flat height="64">
-        <v-toolbar-title>PlaceholderName</v-toolbar-title>
-        <v-spacer/>
-        <logo class="logo"/>
-        <v-toolbar-title>Flat</v-toolbar-title>
-        <v-spacer/>
-        <v-toolbar-items>
-          <v-btn flat>Placeholder1</v-btn>
-          <DatePicker pickerType="month" v-on:picked-date-change="onPickedDateChange"/>
-        </v-toolbar-items>
+      <v-toolbar-title>PlaceholderName</v-toolbar-title>
+      <v-spacer/>
+      <logo class="logo"/>
+      <v-toolbar-title>Flat</v-toolbar-title>
+      <v-spacer/>
+      <v-toolbar-items>
+        <v-btn flat>Placeholder1</v-btn>
+        <DatePicker pickerType="month" v-on:picked-date-change="onPickedDateChange"/>
+      </v-toolbar-items>
     </v-toolbar>
-    <v-layout><router-view/></v-layout>
+    <v-layout>
+      <router-view/>
+    </v-layout>
   </div>
 </template>
 
@@ -73,8 +75,16 @@ export default {
       userName: 'Random User',
       userAvatar: 'https://i.stack.imgur.com/34AD2.jpg',
       items: [
-        { title: 'Expenses', icon: 'trending_down', action: () => true },
-        { title: 'Incomes', icon: 'trending_up', action: () => true },
+        {
+          title: 'Expenses',
+          icon: 'trending_down',
+          action: () => this.$router.push({ name: 'Expenses' }),
+        },
+        {
+          title: 'Revenues',
+          icon: 'trending_up',
+          action: () => this.$router.push({ name: 'Revenues' }),
+        },
         { title: 'Transfers', icon: 'compare_arrows', action: () => true },
         { title: 'About', icon: 'question_answer', action: () => true },
       ],
@@ -90,15 +100,17 @@ export default {
 </script>
 
 <style>
-.rotated{
-  transform: rotate(180deg);
-}
-.logo{
-  height: 2em;
-  width: 2em;
-}
-.nav-toggle-button{
-  position: absolute;
-  bottom: 0;
-}
+  .rotated {
+    transform: rotate(180deg);
+  }
+
+  .logo {
+    height: 2em;
+    width: 2em;
+  }
+
+  .nav-toggle-button {
+    position: absolute;
+    bottom: 0;
+  }
 </style>
